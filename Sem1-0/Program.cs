8957ef71c79a9be3cf602ba42cbb6d71c0bee196
tristorno <tristorn@mail.ru>
2023-02-11 22:35:38 +0300

    Версия 1

---
+++ Sem1-0/Program.cs
@@ -0,0 +1,12 @@
+﻿//Считать введенное число и возвратить его квадрат
+
+Console.WriteLine ("Введите число: ");
+string? inputNum = Console.ReadLine();	//считать из консоли
+int result; //объявил переменную в которую запишу пезультат умножения
+if (inputNum != null)	//проверяю чтобы ввод был не пустым
+{
+	int number = int.Parse (inputNum); //Парсим введенное число
+	result = number * number;      	//Нахожу квадрат
+	//Делаю вывод:
+	Console.WriteLine ("Квадрат этого числа равен: "+result);
+}
