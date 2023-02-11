b7a48cf0a351216f3b26ee1c02160223741e46e8
tristorno <tristorn@mail.ru>
2023-02-11 23:42:11 +0300

    Мое решение задачи

---
+++ Sem1-1/Program.cs
@@ -0,0 +1,13 @@
+﻿// Принимает на вход 2 числа и проверяет, не является ли первое квадратом второго
+
+Console.WriteLine ("Введи первое число: ");
+int num1 = int.Parse (Console.ReadLine ());
+Console.WriteLine ("Введи второе число: ");
+int num2 = int.Parse (Console.ReadLine ());
+if (num1 == num2 * num2)
+{
+	Console.WriteLine ("ЯВЛЯЕТСЯ");
+}
+else
+Console.WriteLine ("НЕ ЯВЛЯЕТСЯ");	
+
