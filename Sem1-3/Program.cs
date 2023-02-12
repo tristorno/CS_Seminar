8a26f3d41a593b829650bd955b0a152b4b7e7187
tristorno <tristorn@mail.ru>
2023-02-12 21:11:19 +0300

    my version

---
+++ Sem1-3/Program.cs
@@ -0,0 +1,14 @@
+﻿//Прогорамма показываеь день недели по ее номеру
+//
+Console.WriteLine ("Введите число:");
+int num = Convert.ToInt16 (Console.ReadLine ());
+string [] weekDay = new string [7];
+weekDay [1] = "понедельник"; 
+weekDay [2] = "вторник";
+weekDay [3] = "среда";
+weekDay [4] = "четверг";
+weekDay [5] = "пятница";
+weekDay [6] = "суббота";
+weekDay [0] = "воскресенье";
+int dayIndex = num % 7;
+Console.WriteLine (weekDay [dayIndex]);
