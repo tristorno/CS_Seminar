a237d406002070cb77aa09c16ab26c07aa876fd2
tristorno <tristorn@mail.ru>
2023-02-12 22:49:02 +0300

    Решение

---
+++ Sem1-7/Program.cs
@@ -0,0 +1,5 @@
+﻿// Принимает на вход трехзначное число и выводит последнее число
+
+Console.WriteLine("Введите число:");
+int number = Convert.ToInt16 (Console.ReadLine ());
+Console.WriteLine (number % 10);
