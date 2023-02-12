128e46c619f2e42e068c0a4688719d9eda4eb362
tristorno <tristorn@mail.ru>
2023-02-12 22:13:52 +0300

    + решение преподавателя

--- Sem1-5/Program.cs
+++ Sem1-5/Program.cs
@@ -5,6 +5,15 @@ int endNum = Convert.ToInt32 (Console.ReadLine ());
 int mediaNum = endNum - 0;
 int beginNum = endNum - mediaNum * 2;
 int count = beginNum;
+//Решение преподавателя.
+//string res = String.Empty
+//while (count <= endnum)
+//{
+//res = res + count + " ";
+//count ++;
+//}
+
+
 while (count <= endNum)
 {
 	Console.WriteLine (count);
