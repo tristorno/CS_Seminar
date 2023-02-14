aba537a4d8982e0705144b89f0f9816a42b14eb8
tristorno <tristorn@mail.ru>
2023-02-14 19:18:54 +0300

    Добавил задачу 1-6

---
+++ Sem1-6/Program.cs
@@ -0,0 +1,11 @@
+﻿//Программа принимает на вход число и проверяет его на четность. На выход отдает "да" или "нет"
+Console.WriteLine("Введите число для проверки его четности:");
+int num = Convert.ToInt32 (Console.ReadLine ());
+if (num % 2 == 0) 
+{
+	Console.WriteLine ("да");
+}	
+else
+{
+	Console.WriteLine ("нет");
+}
