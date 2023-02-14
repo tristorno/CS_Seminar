738b932967dc8c46bb06afd950e3f771c4eb687e
tristorno <tristorn@mail.ru>
2023-02-14 20:41:37 +0300

    Задание 1-8

---
+++ Sem1-8/Program.cs
@@ -0,0 +1,23 @@
+﻿//Получает на вход число и выводит все четные числа от 1 до него 
+Console.WriteLine("Введите положительное число:");
+string? numLine = Console.ReadLine ();
+if (numLine != null)
+{
+	int num = Convert.ToInt32 (numLine);
+	if (num < 1)
+	{
+		Console.WriteLine ("Ошибка!!!	" + numLine + " - не положительное чмсло!");
+	}
+	else
+	{
+		string resLine = String.Empty;
+		int count = 2;
+		while (count <= num)
+		{
+			resLine = (resLine + " " + Convert.ToString (count) + ",");
+			count = count + 2;
+		}
+		Console.WriteLine (resLine + ".");
+	}
+}
+
